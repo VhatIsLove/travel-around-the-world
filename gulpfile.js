86548945ec7568b737f9b3a074c7dc02bbe88f60
Danil-27 <pde200126@gmail.com>
2024-02-01 20:05:38 +0300

    it worked

--- gulpfile.js
+++ gulpfile.js
@@ -2,10 +2,8 @@ const { src, dest } = require('gulp');
 
 const scss = require('gulp-sass')(require('sass'));
 
-function styles(){
-    return src('app/scss/style.scss')
-    .pipe(scss())
-    .pipe(dest('app/css'))
+function styles() {
+	return src('app/scss/style.scss').pipe(scss()).pipe(dest('app/css'));
 }
 
 exports.styles = styles;
